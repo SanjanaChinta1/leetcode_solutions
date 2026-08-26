d7f808b000eca733f6cd084b42fb085780284cc3
SanjanaChinta1 <161240247+SanjanaChinta1@users.noreply.github.com>
2026-08-26 07:22:34 +0000

    2904 code

---
+++ 2904_Shortest_Beautiful_Substring.py
@@ -0,0 +1,30 @@
+# Problem: Shortest and Lexicographically Smallest Beautiful String
+# LeetCode: 2904
+# Difficulty: Medium
+# Approach: Sliding Window
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+from typing import List
+
+class Solution:
+    def shortestBeautifulSubstring(self, s: str, k: int) -> str:
+        l, ones, minlen = 0, 0, float('inf')
+        ans = ""
+
+        for r in range(len(s)):
+            if s[r] == '1':
+                ones += 1
+
+            while ones == k:
+                w = s[l:r + 1]
+
+                if len(w) < minlen or (len(w) == minlen and w < ans):
+                    ans = w
+                    minlen = len(w)
+
+                if s[l] == '1':
+                    ones -= 1
+                l += 1
+
+        return ans
